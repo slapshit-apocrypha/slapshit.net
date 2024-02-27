a5328597039abe1cd464484ff2700fd02ca4088b
Max Reiter <max.m.reiter@gmail.com>
2024-02-27 11:18:39 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: "selector",
+  theme: {
+    extend: {
+      colors: {
+        apocrypha: "#f6611f"
+      },
+    },
+  },
+  plugins: [],
+}
+
